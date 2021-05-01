106ff52051cea693f722ea1cca1befddc0d39d60
Ryan O'Donohue <rod.odonohue@gmail.com>
2021-05-01 10:23:26 -0400

    finsihed product

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const path = require('path');
 
@@ -7,10 +8,24 @@ const PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const reservations = [];
+
+
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
 
-const reservations = [];
-const waitList = [];
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
 
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+
+app.get('/api/reserve', (req, res) => res.json(reservations))
+
+app.post('/api/reserve', (req, res) => {
+    const newReservation = req.body;
+    
+    reservations.push(newReservation)
+   
+    res.json(reservations)    
+})
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
